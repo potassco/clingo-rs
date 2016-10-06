f6880bbfeda7396c2156f5745c87506a4b81335c
sthiele <sthiele78@gmail.com>
2016-10-06 11:42:09 +0200

    make version example same as in the clingo C API doc

--- src/version.rs
+++ src/version.rs
@@ -3,5 +3,5 @@ extern crate clingo;
 fn main() {
 
    let (mo,mi,re) = clingo::safe_clingo_version();
-    println!("clingo version is: {}.{}.{}", mo, mi, re);
+   println!("Hello, this is clingo version {}.{}.{}.", mo, mi, re);
 }
