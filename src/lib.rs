ca0cc601b091355dac1e5c532c62faac8edfa975
Sven Thiele <sthiele78@gmail.com>
2023-10-06 08:07:22 +0200

    Bump to dev version 0.7.3-dev

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/clingo/0.7.2")]
+#![doc(html_root_url = "https://docs.rs/clingo/0.7.3-dev")]
 #![doc = include_str!("../README.md")]
 #![allow(non_upper_case_globals)]
 #![allow(clippy::try_err)]
